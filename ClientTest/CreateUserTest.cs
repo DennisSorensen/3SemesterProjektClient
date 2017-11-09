88408c02121108dffd3b354f612760f42b9f9fa3
Dennis Sørensen <dennissorensen24@hotmail.dk>
2017-11-09 12:29:01 +0100

    Oprettet create user test

---
+++ ClientTest/CreateUserTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClientTest
+{
+    [TestClass]
+    public class CreateUserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
