9abb50b6b0ffd6f93168af3a6abbb69db4fdebbe
Jesper Hesthaven <Junkus@live.dk>
2017-11-09 13:37:00 +0100

    Added Login and Admin from. Added CreateCalendarTest.

---
+++ ClientTest/CreateCalendarTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClientTest
+{
+    [TestClass]
+    public class CreateCalendarTest
+    {
+        [TestMethod]
+        public void TestCalendarIsCreated()
+        {
+            //Arrange
+            CalendarController calendarController = new CalendarController();
+
+            //Act
+            calendarController.Create("TestCalendarName");
+
+            Calendar testCalendarCreate = new Calendar("TestCalendarName");
+
+            //Assert
+            Calendar calendar = calendarController.Get();
+            Assert.ReferenceEquals(calendar, testCalendar);
+            
+        }
+
+
+        [TestMethod]
+        public void TestCalendarIdentical()
+        {
+            //Arrange
+            CalendarController calendarController = new CalendarController();
+
+            //Act
+            calendarController.Create("TestCalendarName");
+
+            Calendar testCalendarIndentical1 = new Calendar("TestCalendarIdentical");
+            Calendar testCalendarIndentical2 = new Calendar("TestCalendarIdentical");
+
+            //Assert
+            Calendar calendar = calendarController.Get();
+            Assert.ReferenceEquals(calendar, testCalendar);
+
+        }
+
+    }
+}
