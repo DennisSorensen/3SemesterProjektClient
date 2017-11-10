0f832dfc7f7d03dcd514bde0cd4789e38bcad882
Dennis Sørensen <dennissorensen24@hotmail.dk>
2017-11-10 11:49:11 +0100

    Lavet instans af servicereference

--- Client/UserManagement.cs
+++ Client/UserManagement.cs
@@ -19,10 +19,10 @@ namespace Client
 
         //Laver en instans af vores service reference, sådan vi kan kalde dem
         ServiceReference1.IService1 service = new ServiceReference1.Service1Client();
-
+        
         private void UserManagement_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
