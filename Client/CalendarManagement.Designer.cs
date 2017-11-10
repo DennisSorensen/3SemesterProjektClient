63046a211bc78282dde50e9e013db518476060c3
Bodil <bodil.noergaard86@gmail.com>
2017-11-10 12:18:19 +0100

    nyredigeret calendar management

--- Client/CalendarManagement.Designer.cs
+++ Client/CalendarManagement.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabCreateCal = new System.Windows.Forms.TabPage();
-            this.lblTitleUser = new System.Windows.Forms.Label();
             this.tabViewCal = new System.Windows.Forms.TabPage();
             this.tabEditCal = new System.Windows.Forms.TabPage();
             this.tabDeleteCal = new System.Windows.Forms.TabPage();
             this.btnCreateCal = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabCreateCal.SuspendLayout();
             this.SuspendLayout();
@@ -55,9 +56,10 @@
             // 
             // tabCreateCal
             // 
-            this.tabCreateCal.Controls.Add(this.comboBox1);
+            this.tabCreateCal.Controls.Add(this.label2);
+            this.tabCreateCal.Controls.Add(this.listBox1);
+            this.tabCreateCal.Controls.Add(this.label1);
             this.tabCreateCal.Controls.Add(this.btnCreateCal);
-            this.tabCreateCal.Controls.Add(this.lblTitleUser);
             this.tabCreateCal.Location = new System.Drawing.Point(4, 38);
             this.tabCreateCal.Name = "tabCreateCal";
             this.tabCreateCal.Padding = new System.Windows.Forms.Padding(3);
@@ -65,15 +67,6 @@
             this.tabCreateCal.TabIndex = 0;
             this.tabCreateCal.Text = "Opret Kalender";
             this.tabCreateCal.UseVisualStyleBackColor = true;
-            // 
-            // lblTitleUser
-            // 
-            this.lblTitleUser.AutoSize = true;
-            this.lblTitleUser.Location = new System.Drawing.Point(19, 18);
-            this.lblTitleUser.Name = "lblTitleUser";
-            this.lblTitleUser.Size = new System.Drawing.Size(158, 29);
-            this.lblTitleUser.TabIndex = 1;
-            this.lblTitleUser.Text = "Find brugere:";
             // 
             // tabViewCal
             // 
@@ -108,7 +101,7 @@
             // btnCreateCal
             // 
             this.btnCreateCal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreateCal.Location = new System.Drawing.Point(134, 363);
+            this.btnCreateCal.Location = new System.Drawing.Point(70, 349);
             this.btnCreateCal.Name = "btnCreateCal";
             this.btnCreateCal.Size = new System.Drawing.Size(154, 42);
             this.btnCreateCal.TabIndex = 5;
@@ -116,13 +109,34 @@
             this.btnCreateCal.UseVisualStyleBackColor = true;
             this.btnCreateCal.Click += new System.EventHandler(this.btnCreateCal_Click);
             // 
-            // comboBox1
+            // label1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(24, 60);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(196, 37);
-            this.comboBox1.TabIndex = 6;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Supporter - oversigt";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(34, 120);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 178);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Markér den ønskede supporter";
             // 
             // CalendarManagement
             // 
@@ -145,9 +159,10 @@
         private System.Windows.Forms.TabPage tabCreateCal;
         private System.Windows.Forms.TabPage tabViewCal;
         private System.Windows.Forms.TabPage tabEditCal;
-        private System.Windows.Forms.Label lblTitleUser;
         private System.Windows.Forms.TabPage tabDeleteCal;
         private System.Windows.Forms.Button btnCreateCal;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
