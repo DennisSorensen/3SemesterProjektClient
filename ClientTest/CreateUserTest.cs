edd40081e7de2a93ecc921b2ed7c0b073a27b78f
ann <ann.hvarregaard@gmail.com>
2017-11-14 10:31:48 +0100

    lavet test - usermanagement

--- ClientTest/CreateUserTest.cs
+++ ClientTest/CreateUserTest.cs
@@ -7,8 +7,28 @@ namespace ClientTest
     public class CreateUserTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestUserIsCreated()
         {
+
+            //Arrange
+            //UserController userController = new UserController();
+
+            UserManagement userManagement = new UserManagement(); //Her kalder vi metoden som opretter en user i GUI'en.
+
+            //Act
+            userController.Create("TestUser");
+
+            User testUserCreate = new User("TestUser");
+
+            userManagement.Create(int id, string role, string firstname, string lastname, string password);
+
+            //Assert
+            User user = userController.Get();
+            Assert.ReferenceEquals(user, testUser);
+
+
         }
     }
 }
+
+
