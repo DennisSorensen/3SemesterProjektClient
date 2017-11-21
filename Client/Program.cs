cc788d5285787c6e52e3cd36389eab493aea648a
Jesper Hesthaven <Junkus@live.dk>
2017-11-21 11:01:40 +0100

    Add

--- Client/Program.cs
+++ Client/Program.cs
@@ -16,7 +16,7 @@ namespace Client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SupporterCalendar());
+            Application.Run(new AdminClientFront());
         }
     }
 }
