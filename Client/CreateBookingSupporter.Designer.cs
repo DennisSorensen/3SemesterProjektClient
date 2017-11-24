5d26ad123e8b0f19a4ac57542e6d9278d10a5568
Dennis Sørensen <dennissorensen24@hotmail.dk>
2017-11-24 11:30:47 +0100

    blahh

--- Client/CreateBookingSupporter.Designer.cs
+++ Client/CreateBookingSupporter.Designer.cs
@@ -28,11 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbTaskStatDate = new System.Windows.Forms.ComboBox();
+            this.cbTaskEndDate = new System.Windows.Forms.ComboBox();
+            this.txtTaskName = new System.Windows.Forms.TextBox();
+            this.txtTaskDescription = new System.Windows.Forms.TextBox();
+            this.btnCreateTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Opret opgave";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Start Tid:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Slut Tid:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Opgave navn:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = " Beskrivelse:";
+            // 
+            // cbTaskStatDate
+            // 
+            this.cbTaskStatDate.FormattingEnabled = true;
+            this.cbTaskStatDate.Location = new System.Drawing.Point(159, 103);
+            this.cbTaskStatDate.Name = "cbTaskStatDate";
+            this.cbTaskStatDate.Size = new System.Drawing.Size(121, 24);
+            this.cbTaskStatDate.TabIndex = 5;
+            // 
+            // cbTaskEndDate
+            // 
+            this.cbTaskEndDate.FormattingEnabled = true;
+            this.cbTaskEndDate.Location = new System.Drawing.Point(159, 173);
+            this.cbTaskEndDate.Name = "cbTaskEndDate";
+            this.cbTaskEndDate.Size = new System.Drawing.Size(121, 24);
+            this.cbTaskEndDate.TabIndex = 6;
+            // 
+            // txtTaskName
+            // 
+            this.txtTaskName.Location = new System.Drawing.Point(172, 216);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.Size = new System.Drawing.Size(100, 22);
+            this.txtTaskName.TabIndex = 7;
+            // 
+            // txtTaskDescription
+            // 
+            this.txtTaskDescription.Location = new System.Drawing.Point(172, 250);
+            this.txtTaskDescription.Name = "txtTaskDescription";
+            this.txtTaskDescription.Size = new System.Drawing.Size(100, 22);
+            this.txtTaskDescription.TabIndex = 8;
+            // 
+            // btnCreateTask
+            // 
+            this.btnCreateTask.Location = new System.Drawing.Point(138, 320);
+            this.btnCreateTask.Name = "btnCreateTask";
+            this.btnCreateTask.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateTask.TabIndex = 9;
+            this.btnCreateTask.Text = "Opret";
+            this.btnCreateTask.UseVisualStyleBackColor = true;
+            this.btnCreateTask.Click += new System.EventHandler(this.btnCreateTask_Click);
+            // 
+            // CreateBookingSupporter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 418);
+            this.Controls.Add(this.btnCreateTask);
+            this.Controls.Add(this.txtTaskDescription);
+            this.Controls.Add(this.txtTaskName);
+            this.Controls.Add(this.cbTaskEndDate);
+            this.Controls.Add(this.cbTaskStatDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateBookingSupporter";
             this.Text = "CreateBookingSupporter";
+            this.Load += new System.EventHandler(this.CreateBookingSupporter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbTaskStatDate;
+        private System.Windows.Forms.ComboBox cbTaskEndDate;
+        private System.Windows.Forms.TextBox txtTaskName;
+        private System.Windows.Forms.TextBox txtTaskDescription;
+        private System.Windows.Forms.Button btnCreateTask;
     }
 }
--- Client/CreateBookingSupporter.cs
+++ Client/CreateBookingSupporter.cs
@@ -12,9 +12,36 @@ namespace Client
 {
     public partial class CreateBookingSupporter : Form
     {
-        public CreateBookingSupporter()
+        BookingService bookingService = new BookingService();
+        CalendarService calendarService = new CalendarService();
+
+        int userId = 2;
+
+       public CreateBookingSupporter(/*int userId*/)
         {
             InitializeComponent();
+            //this.userId = userId;
+        }
+
+        private void btnCreateTask_Click(object sender, EventArgs e)
+        {
+            BookingServiceReference.SupportTask supportTask = new BookingServiceReference.SupportTask();
+            supportTask.StartDate = Convert.ToDateTime(cbTaskStatDate.Text);
+            supportTask.EndDate = Convert.ToDateTime(cbTaskEndDate.Text);
+            supportTask.Name = txtTaskName.Text;
+            supportTask.Description = txtTaskDescription.Text;
+            CalendarServiceReference.Calendar calendar = calendarService.Get(userId);
+            supportTask.Calendar_Id = calendar.Id;
+            supportTask.Id = userId;
+            supportTask.BookingType = "Task";
+
+            bookingService.CreateTask(supportTask);
+            
+        }
+
+        private void CreateBookingSupporter_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
